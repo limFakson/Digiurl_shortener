e5392db189b786edca802e891dd4f780a8dc3c4c
limfakson <fakeyejoshua2005@gmail.com>
2024-08-11 04:36:17 +0100

    edit update(1)

--- shortener/settings.py
+++ shortener/settings.py
@@ -176,7 +176,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'view', 'theme', 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'view', 'theme', 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
